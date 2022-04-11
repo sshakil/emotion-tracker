8b3e744eaaff796934c74f2c04abd22e6a9682a8
Saad <sshakil@users.noreply.github.com>
2022-04-10 23:23:50 -0400

    remove all migrations

--- db/migrate/20220325021359_remove_date_from_periods.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDateFromPeriods < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :periods, :date
-  end
-end
--- db/migrate/20220405033312_remove_date_from_day_again.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDateFromDayAgain < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :days, :date, :datetime
-  end
-end
